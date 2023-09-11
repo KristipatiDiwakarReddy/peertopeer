6cafe9dd5b859788a9a4f1dffddc41112627f42a
KristipatiDiwakarReddy <95432925+KristipatiDiwakarReddy@users.noreply.github.com>
2023-09-12 00:21:19 +0530

    Update code.js
    removed "," in buffersize

--- public/code.js
+++ public/code.js
@@ -43,7 +43,7 @@
 			shareFile({
 				filename: file.name,
 				total_buffer_size:buffer.length,
-				buffer_size:56,320,
+				buffer_size:56320,
 			}, buffer, el.querySelector(".progress"));
 		}
 		reader.readAsArrayBuffer(file);
